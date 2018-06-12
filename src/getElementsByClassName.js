cf208a665f3fa4807e5e8647591a16904e84aaee
rroque98 <rroque98@gmail.com>
2018-06-11 20:23:39 -0700

    finished bare min reqs. All tests pass for getElementsByClassName and StringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,44 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  
+
+  // break down document.body
+  // recursively break it down into smaller pieces
+  // if the ... is equal to className
+  var checkForClass = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+  };
+
+  var traverseDom = function(elem) {
+    if (elem.childNodes.length === 0) {
+      checkForClass(elem);
+    } else {
+      checkForClass(elem);
+      for (var i = 0; i < elem.childNodes.length; i++) {
+        traverseDom(elem.childNodes[i]);
+      }
+    }
+  };
+
+  traverseDom(document.body);
+  return result;
 };
+
+// input = string
+// output = array of elements with a class name
+// strategy
+// Run on the document.body.
+// traverse the DOM.
+// pseudocode
+  // start at document.body
+  // declare array to store elements
+  // use jquery to locate first instance of desired class name
+  // if body.child has the class name
+    // push element into result
+  // else
+    // check children
